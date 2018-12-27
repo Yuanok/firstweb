20d02c939418f9918bd2219505c20da5443a29e1
Yuanok <382547456@qq.com>
2018-12-27 23:42:46 +0800

    用户界面 - 商品加入购物车

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable
                     ->withTimestamps()
                     ->orderBy('user_favorite_products.created_at','desc');
     }
+
+//    public  function cartItems(){
+//        return $this->hasMany(CartItem::class);
+//    }
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
