5ce98d39ccced0b2f9cdbb0ac6057175864a5460
Yuanok <382547456@qq.com>
2018-12-27 16:49:41 +0800

    用户界面 - 商品收藏

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -36,7 +36,11 @@
                             </div>
                             <div class="cart_amount"><label for="">数量</label> <input type="text" class="form-control input-sm" value="1"><span>件</span><span class="stock"></span></div>
                             <div class="buttons">
-                                <button class="btn btn-success btn-favor">❤收藏</button>
+                                @if($favored)
+                                    <button class="btn btn-danger btn-disfavor">❤取消收藏</button>
+                                @else
+                                    <button class="btn btn-success btn-favor">❤收藏</button>
+                                @endif
                                 <button class="btn btn-primary btn-add-to-cart">加入购物车</button>
                             </div>
                         </div>
@@ -66,6 +70,38 @@
             $('.sku-btn').click(function () {
                 $('.product-info .price span').text($(this).data('price'));
                 $('.product-info .stock').text('库存：' + $(this).data('stock') + '件');
+            });
+            //监听收藏按钮的点击事件
+            $('.btn-favor').click(function () {
+                // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成
+                axios.post('{{ route('products.favor',['product' =>$product->id]) }}')
+                    .then(function () {//请求成功返回执行这个回调
+                        swal('操作成功','','success')
+                            .then(function () {
+                                location.reload();
+                            });
+                    },function (error) {//请求失败会执行这个回调
+                        //如果返回码401是代表没登
+                        if(error.response && error.response.status ===401){
+                            swal('请先登录','error');
+                        }else if(error.response && error.response.data.msg){
+                            //其他有msg字段的情况，将msg提示给用户
+                            swal(error.response.data.msg,'','error');
+                        }else{
+                            //其他情况
+                            swal('系统错误','','error');
+                        }
+                    });
+            });
+
+            $('.btn-disfavor').click(function (){
+                axios.delete('{{route('products.disfavor',['prodyct'=>$product->id]) }}')
+                    .then(function () {
+                        swal('操作成功','','success')
+                            .then(function(){//这里加一个then方法
+                                location.reload();
+                            });
+                    });
             });
         });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,25 @@
 
 Auth::routes();
 
-Route::group(['middleware'=>'auth'],function(){
-    Route::get('/email_verify_notice','PagesController@emailVerifyNotice')
+Route::group(['middleware'=>'auth'],function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')
         ->name('email_verify_notice');
-    Route::get('/email_verification/verify','EmailVerificationController@verify')
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')
         ->name('email_verification.verify');
-    Route::get('/email_verification/send','EmailVerificationController@send')
+    Route::get('/email_verification/send', 'EmailVerificationController@send')
         ->name('email_verification.send');
 
-    Route::group(['middleware'=>'email_verified'],function(){
-        Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
-        Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
-        Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
-        Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
-        Route::put('user_addresses/{user_address}','UserAddressesController@update')->name('user_addresses.update');
-        Route::delete('user_addresses/{user_address}','UserAddressesController@destroy')->name('user_addresses.destroy');
+    Route::group(['middleware' => 'email_verified'], function () {
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     });
 });
-
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}','ProductsController@show')->name('products.show');
